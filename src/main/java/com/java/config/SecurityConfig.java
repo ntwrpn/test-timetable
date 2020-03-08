dafbcc9012db8c6956ce7862879bed6971c0114c
ntwrpn <ntwrpn@users.noreply.github.com>
2020-03-08 12:18:57 +0300

    add addResourceHandler

--- src/main/java/com/java/config/SecurityConfig.java
+++ src/main/java/com/java/config/SecurityConfig.java
@@ -27,8 +27,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	DataSource dataSource;
+    @Autowired
+    DataSource dataSource;
 
 //    @Autowired
 //    private RestAuthenticationEntryPoint authenticationEntryPoint;
@@ -42,8 +42,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/registration/", "/fonts/**", "/resources/**").permitAll()
-                .anyRequest().authenticated().and().formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password").permitAll().and().logout().permitAll();
+        http.authorizeRequests().antMatchers("/", "/registration/", "/resources/**").permitAll()
+        .anyRequest().authenticated().and().formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password").permitAll().and().logout().permitAll();
         http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();//.authenticationEntryPoint(authenticationEntryPoint);
         http.exceptionHandling().accessDeniedPage("/403");
 
@@ -53,7 +53,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 
     private Map<String, ?> hibernateJpaProperties() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
