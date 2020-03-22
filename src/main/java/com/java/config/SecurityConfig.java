9ea6e7d8b440ebcf13f467ae18e3a2a57cebd9f3
Stas <stavakalov@inbox.ru>
2020-03-22 15:42:06 +0300

    fix SecurityConfig, deleted unnecessary

--- src/main/java/com/java/config/SecurityConfig.java
+++ src/main/java/com/java/config/SecurityConfig.java
@@ -3,27 +3,19 @@ package com.java.config;
 import java.util.Map;
 import javax.sql.DataSource;
 
-import com.java.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.authentication.AuthenticationManager;
-import com.java.config.CustomUserDetailsService;
 
 
 @Configuration
@@ -38,25 +30,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     DataSource dataSource;
 
-    @Autowired
-    private JwtAuthEntryPoint unauthorizedHandler;
- 
     @Bean
     public JwtAuthTokenFilter authenticationJwtTokenFilter() {
         return new JwtAuthTokenFilter();
     }
-    
-    
-//    @Autowired
-//    private RestAuthenticationEntryPoint authenticationEntryPoint;
 
-    /*@Autowired
-    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource)
-				.usersByUsernameQuery("select username,password, enabled from users where username=?").passwordEncoder(new BCryptPasswordEncoder())
-				.authoritiesByUsernameQuery("SELECT T3.username, T2.role FROM users_roles as T1 INNER JOIN users as T3 ON T3.username=? INNER JOIN user_roles as T2 ON T1.user_role_id=T2.id WHERE T1.users=T3.id;");
-    }*/
-    
     @Override
     public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         authenticationManagerBuilder
@@ -73,11 +51,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests().antMatchers("/api/auth/**", "/", "/registration/", "/resources/**").permitAll()
-        .anyRequest().authenticated().and().formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password").permitAll().and().logout().permitAll();
-        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();//.authenticationEntryPoint(authenticationEntryPoint);
+        http.authorizeRequests().antMatchers("/api/auth/**", "/", "/registration/", "/resources/**").permitAll();
+        http.cors().and().csrf().disable();
         http.exceptionHandling().accessDeniedPage("/403");
-
         
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
