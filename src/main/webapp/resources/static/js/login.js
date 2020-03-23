75e97408a3ec0bacfa4f7f3155a22ee3fea98993
ntwrpn <ntwrpn@users.noreply.github.com>
2020-03-23 19:52:37 +0300

    login fix
    login fix

--- src/main/webapp/resources/static/js/login.js
+++ src/main/webapp/resources/static/js/login.js
@@ -4,3 +4,33 @@ $(document).ready(() => {
     $('.modal').modal();
     $("#login-modal").modal("open");
 });
+
+
+
+const LoginClick = () => {
+    let json = getJSONfromForm("login-form");
+    let regform = {
+        username: json[0]['value'],
+        password: json[1]['value']
+    }
+
+    $.ajax({
+    headers: { 
+        'Accept': 'application/json',
+        'Content-Type': 'application/json' 
+    },
+    'type': 'POST',
+    'url': "/api/auth/signin/",
+    'data': JSON.stringify(regform),
+    'dataType': 'json',
+    'success': function(response) {
+        window.location.href = "/";
+    }
+});
+    
+}
+
+const getJSONfromForm = (formname) => {
+    let formData = $("#"+formname).serializeArray();
+    return formData;
+}
